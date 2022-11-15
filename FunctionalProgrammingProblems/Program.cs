449b8c3bb9e5811464907d1d81aa11249ae305c5
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-15 08:54:50 +0530

    Added Welcome Message For FunctionalProgrammingProblems

---
+++ FunctionalProgrammingProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalProgrammingProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To FunctionalProgrammingProblems");
+        }
+    }
+}
