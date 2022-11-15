37e97243ee3ff796f91b93085c65ac3477631ab1
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-15 09:00:37 +0530

    Added FlipCoin Problem in FunctionalProgrammingProblems

---
+++ FunctionalProgrammingProblems/FlipCoin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgrammingProblems
+{
+    public static class FlipCoin
+    {
+        public static void HeadOrTailPercentage()
+        {
+            int TailCount = 0;
+            int HeadCount = 0;
+            Double HeadPercentage = 0;
+            Double TailPercentage = 0;
+
+            Console.WriteLine("Enter The No Of times to Flips");
+            int flips = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < flips; i++)
+            {
+                Random random = new Random();
+                int num = random.Next(2);
+
+                if (num == 0)
+                {
+                    Console.WriteLine("Tail");
+                    TailCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Head");
+                    HeadCount++;
+                }
+            }
+            HeadPercentage = (HeadCount * 100) / flips;
+            TailPercentage = (TailCount * 100) / flips;
+            Console.WriteLine("tailpercentage " + TailPercentage);
+            Console.WriteLine("headpercentage " + HeadPercentage);
+        }
+    }
+}
